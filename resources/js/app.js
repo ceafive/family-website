2f2f2178a912ae4838da131e7b402fbfc973eb70
Castro <ceafive@gmail.com>
2019-11-08 16:47:23 +0000

    weather added

--- resources/js/app.js
+++ resources/js/app.js
@@ -46,36 +46,20 @@ const second = 1000,
           hoursDisplay = Math.floor((distance % (day)) / (hour)),
           minutesDisplay = Math.floor((distance % (hour)) / (minute)),
           secondsDisplay = Math.floor((distance % (minute)) / second),
-          days = document.getElementById('days'),
-          hours = document.getElementById('hours'),
-          minutes = document.getElementById('minutes'),
-          seconds = document.getElementById('seconds');
+          days = document.getElementById('clock').children[0],
+          hours = document.getElementById('clock').children[1],
+          minutes = document.getElementById('clock').children[2],
+          seconds = document.getElementById('clock').children[3];
           
 //Inputting countdown into div with ID clock //
           days.children[0].innerText = daysDisplay;
+          (daysDisplay < 2) ? days.children[1].innerText = "day" : days.children[1].innerText = "days";
           hours.children[0].innerText = hoursDisplay;
+          (hoursDisplay < 2) ? hours.children[1].innerText = "hour" : hours.children[1].innerText = "hours";
           minutes.children[0].innerText = minutesDisplay;
+          (minutesDisplay < 2) ? minutes.children[1].innerText = "minute" : minutes.children[1].innerText = "minutes";
           seconds.children[0].innerText = secondsDisplay;
-          if (daysDisplay < 2){
-            days.children[1].innerText = "day"
-          }else {
-            days.children[1].innerText = "days"
-          }
-          if (hoursDisplay < 2){
-            hours.children[1].innerText = "hour"
-          }else {
-            hours.children[1].innerText = "hours"
-          }
-          if (minutesDisplay < 2){
-            minutes.children[1].innerText = "minute"
-          }else {
-            minutes.children[1].innerText = "minutes"
-          }
-          if (secondsDisplay < 2){
-            seconds.children[1].innerText = "second"
-          }else {
-            seconds.children[1].innerText = "seconds"
-          }
+          (secondsDisplay < 2) ? seconds.children[1].innerText = "second" : seconds.children[1].innerText = "seconds";
          
 
     //Logic for running a function when the clock runs down to one month left
@@ -91,6 +75,7 @@ const second = 1000,
             clock.classList.add('hidden');
             button.innerText = "Directions to Venue!"
             link.href = "https://www.google.com/maps/place/5%C2%B032'59.0%22N+0%C2%B014'02.0%22W/@5.549712,-0.2344212,19z/data=!3m1!4b1!4m6!3m5!1s0x0:0x0!7e2!8m2!3d5.5497123!4d-0.2338741"
+            link.target ="_blank";
             main.classList.add('xxs:pt-32');
             main.classList.remove('xxs:pt-2', 'xs:pt-16', 'md:pt-24', 'xl:pt-12');
     //Function to stop countdown
