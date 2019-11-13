53929d61bb5ce32c4c4de1c1233e22e09b836562
Castro <ceafive@gmail.com>
2019-11-13 15:42:09 +0000

    rsvp.js update

--- resources/js/rsvp.js
+++ resources/js/rsvp.js
@@ -21,7 +21,7 @@ code.addEventListener('keypress', function (e) {
         map_tag.classList.remove('xxs:mt-16', 'xl:mt-56')
         map.classList.remove('hidden')
         map.classList.add('block')
-    }else if ((key.length < 6 && code.value !== "260688")){
+    }else if (key === 13 && code.value.length === 6  && code.value !== "260688"){
         alert("Wrong Code Entered")
     }
 });
