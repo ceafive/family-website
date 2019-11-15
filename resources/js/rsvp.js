2b177713c67bf10a97acd793142ba177a8381846
Castro <ceafive@gmail.com>
2019-11-15 20:48:28 +0000

    rsvp page edited

--- resources/js/rsvp.js
+++ resources/js/rsvp.js
@@ -1,27 +1,29 @@
-const   map_tag = document.getElementById('map-tag');
-const       code = document.getElementById('code');
-const      map = document.getElementById('map');
+// eslint-disable-next-line camelcase
+const map_tag = document.getElementById('map-tag');
+const code = document.getElementById('code');
+const map = document.getElementById('map');
 
-let mapDisplay = () => {
-    map_tag.classList.remove('xxs:mt-16', 'xl:mt-56')
-    code.classList.remove('hidden')
-    code.classList.add('block')
-}
+const mapDisplay = () => {
+  map_tag.classList.remove('xxs:mt-16', 'xl:mt-56');
+  code.classList.remove('hidden');
+  code.classList.add('block');
+};
 
-map_tag.addEventListener('click', mapDisplay)
+map_tag.addEventListener('click', mapDisplay);
 
-code.addEventListener('keypress', function (e) {
-    var key = e.which || e.keyCode;
-    if (key === 13 && code.value === "260688") { 
-        code.classList.add('hidden')
-        code.classList.remove('block')
-        code.value = ""
-        map_tag.removeEventListener('click', mapDisplay)
-        map_tag.innerText = "Click map to open Google Maps"
-        map_tag.classList.remove('xxs:mt-16', 'xl:mt-56', 'cursor-pointer')
-        map.classList.remove('hidden')
-        map.classList.add('block')
-    }else if (key === 13 && code.value.length === 6  && code.value !== "260688"){
-        alert("Wrong Code Entered")
-    }
+code.addEventListener('keypress', function(e) {
+  const key = e.which || e.keyCode;
+  if (key === 13 && code.value === '260688') {
+    code.classList.add('hidden');
+    code.classList.remove('block');
+    code.value = '';
+    map_tag.removeEventListener('click', mapDisplay);
+    map_tag.innerText = 'Click map to open Google Maps';
+    map_tag.classList.remove('xxs:mt-16', 'xl:mt-56', 'cursor-pointer');
+    map.classList.remove('hidden');
+    map.classList.add('block');
+  } else if (key === 13 && code.value.length === 6 && code.value !== '260688') {
+    // eslint-disable-next-line no-alert
+    alert('Wrong Code Entered');
+  }
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,48 +1,48 @@
 module.exports = {
   theme: {
     fontFamily: {
-            'display': ['Playfair Display'],
-            'body': ['Poiret One'],
+      display: ['Playfair Display'],
+      body: ['Poiret One']
     },
     extend: {
-        colors: {
-            appteal: '#0a1c1c',
-            appgold: '#a48623',    
-            white: '#E8E9ED',
-            black: {
-              default: '#000',
-              lighter: "rgba(0,0,0,0.9)"
-            }    
-        },
-        fontSize: {
-            '8xl': '5rem',
-            '10xl': '7rem'
-        },
-        spacing: {
-            '80': '24rem',
-            '128': '32rem',
-        },
-        letterSpacing: {
-            loose: '0.5em',
-            looser: '1.1em',
-        },
-        inset: {
-            "15": "15px",
-            "10": "10px",
+      colors: {
+        appteal: '#0a1c1c',
+        appgold: '#a48623',
+        white: '#E8E9ED',
+        black: {
+          default: '#000',
+          lighter: 'rgba(0,0,0,0.9)'
         }
+      },
+      fontSize: {
+        '8xl': '5rem',
+        '10xl': '7rem'
+      },
+      spacing: {
+        '80': '24rem',
+        '128': '32rem'
+      },
+      letterSpacing: {
+        loose: '0.5em',
+        looser: '1.1em'
+      },
+      inset: {
+        '15': '15px',
+        '10': '10px'
+      }
     },
     screens: {
-            'xxs': '320px',
-            'xs': '375px',
-            'sm': '640px',
-            'md': '768px',
-            'lg': '1024px',
-            'xl': '1280px',
-            'xl2': '1630px'
+      xxs: '320px',
+      xs: '375px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      xl2: '1630px'
     }
   },
   variants: {
-    cursor: ['responsive', 'hover', 'focus'],
+    cursor: ['responsive', 'hover', 'focus']
   },
   plugins: []
-}
+};
